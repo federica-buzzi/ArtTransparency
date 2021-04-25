a7db22ca16aba32c829473229b390940e491e013
Federica <infofedericabuzzi@gmail.com>
2021-04-25 17:40:00 +0200

    integrated template

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/dataviz', name: 'dataviz')]
+    public function dataviz(): Response
+    {
+        return $this->render('home/dataviz.html.twig');
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig');
+    }
+
+    #[Route('/form', name: 'form')]
+    public function form(): Response
+    {
+        return $this->render('home/form.html.twig');
+    }
+}
