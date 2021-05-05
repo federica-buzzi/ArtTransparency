9f68592f0b2405f4edda430989ad6b64b2b48f96
Federica <infofedericabuzzi@gmail.com>
2021-05-05 08:58:53 +0200

    2 types of fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,34 +4,13 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use League\Csv\Reader;
-use App\Entity\UserJob;
 
-class ImporterCSVFixtures extends Fixture
+class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $csv = Reader::createFromPath('%kernel.root_dir%/../src/DataFixtures/csv/art-transparency-data.csv');
-        $csv->setHeaderOffset(0);
-        $header = $csv->getHeader(); //returns the CSV header record
-        $records = $csv->getRecords();
-        foreach ($records as $record) {
-            $record['role']=(string)$record['role']; 
-            $record['startingSalary'] = (float)$record['startingSalary'];
-            $record['contractStartingSalary'] = (string)$record['contractStartingSalary'];
-            $record['yearStartingSalary'] = (int)$record['yearStartingSalary'];
-            $record['endingSalary'] = (float)$record['endingSalary'];
-            $record['contractEndingSalary'] = (string)$record['contractEndingSalary'];
-            $record['yearEndingSalary'] = (int)$record['yearEndingSalary'];
-            $record['typeContract'] = (string)$record['typeContract']; 
-            $record['yearsExperience'] = (int)$record['yearsExperience'];
-            $record['gender'] = (string)$record['gender'];
-            $record['ethnicity'] = (string)$record['ethnicity'];
-            $record['degreesRequired'] = (string)$record['degreesRequired']; 
-
-            $obj = new UserJob($record);
-            $manager->persist($obj);
-        }
+        // $product = new Product();
+        // $manager->persist($product);
 
         $manager->flush();
     }
